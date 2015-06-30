f322d58bb1e1ab639dc3ae472796de6ad02454f2
leroi <leroidejesa@gmail.com>
2015-06-30 15:43:54 -0700

    add Create/Update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,42 @@ class PostsController < ApplicationController
     @posts = Post.all
     render :index
   end
+
+  def show
+    @post = Post.find(params[:id])
+    render :show
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :url)
+  end
 end
