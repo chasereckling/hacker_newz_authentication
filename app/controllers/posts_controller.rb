59e6c67033b07045b28dc6d995430b89c4a803b3
chase reckling & leroi dejesa <student@epicodus.com>
2015-07-07 11:39:21 -0700

    add sorting options to posts index pagefor sorting by points and date, fix vote up to sort by points.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(created_at: :desc)
-    # @current = current_user
+    if params[:sort_by] == "points"
+      @posts = Post.all.sort_by{|object| [object.points]}.reverse
+    elsif params[:sort_by] == "date"
+      @posts = Post.all.sort_by{|object| [object.created_at]}.reverse
+    else
+      @posts = Post.all.sort_by{|object| [object.points, object.created_at]}.reverse
+    end
   end
 
   def show
@@ -10,8 +15,15 @@ class PostsController < ApplicationController
   end
 
   def new
+    @posts = Post.all.order(created_at: :desc)
     @post = Post.new
 
+    if current_user
+      render :new
+    else
+      flash[:alert] = "You must be logged in to post!"
+      render :index
+    end
   end
 
   def create
@@ -34,8 +46,10 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @posts = Post.all.sort_by{|object| [object.points]}.reverse
 
     if @post.update(post_params)
+      # @posts = Post.all.sort_by{|object| [object.points]}.reverse
       redirect_to posts_path
     else
       render :edit
