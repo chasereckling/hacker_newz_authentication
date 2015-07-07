c44992e7d4b96ad2f7b75812c1758e54bdfc0c84
chase reckling & leroi dejesa <student@epicodus.com>
2015-07-07 15:30:51 -0700

    add admin authorization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,15 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def user_admin
+    if current_user
+      if current_user.admin === true
+        @user_admin = current_user
+      end
+    end
+  end
+
   helper_method :current_user
+  helper_method :user_admin
 end
